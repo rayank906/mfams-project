09be391dede8e49b14396a7c98041830d2d2cc66
Rayan Kamdem <rayankdm@umich.edu>
2025-04-04 14:05:17 -0400

    main modif

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-# import requests
-# import pandas as pd
-# from io import StringIO
-
-# # Setting up API call to retrieve Kalshi data
-# api_url = "https://api.elections.kalshi.com/trade-api/v2/api_version"
-
-# headers = {"accept": "application/json"}
-
-# response = requests.get(api_url, headers=headers)
-
-# if response.status_code == 200:
-#     csv_data = StringIO(response.text)
-#     df = pd.read_csv(csv_data)
-#     print(df.head())
-# else:
-#     print(f"Failed to fetch data. Status Code: {response.status_code}")
-
 import requests
+import pandas as pd
+from io import StringIO
 
-url = "https://api.elections.kalshi.com/trade-api/v2/api_version"
+# Setting up API call to retrieve Kalshi data
+api_url = "https://api.elections.kalshi.com/trade-api/v2/api_version"
 
-headers = {"accept": "application/json"}
+headers = {"accept": "319b80cd-7ba7-40c2-ba22-4972bf48d75e"}
 
-response = requests.get(url, headers=headers)
+response = requests.get(api_url, headers=headers)
 
-print(response.text)
+if response.status_code == 200:
+    csv_data = StringIO(response.text)
+    df = pd.read_csv(csv_data)
+    print(df.head())
+else:
+    print(f"Failed to fetch data. Status Code: {response.status_code}")
